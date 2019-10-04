83888d9f5f62835ce91cb5ed887df132292eefbf
Savvas Sheittanis <sheittanis@hotmail.com>
2019-10-04 22:08:32 +0100

    Peek Articles

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,23 +1,33 @@
 
 import React from 'react'
-import { Header, Segment, Grid, Image } from 'semantic-ui-react'
+import { Header, Segment, Grid, Image, Popup, Icon } from 'semantic-ui-react'
+
 import PreviewExample from "../images/previews/previewTemplate.jpg"
 import { loremIpsum, getCurrentDate } from "../utils"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
-const ArticleCard = (props) => {
+const ArticleCard = () => {
 
     return (
         <Segment className="articleCard" style={{ margin: '1rem' }}>
             <Grid>
                 <Grid.Row columns={2}>
                     <Grid.Column >
-                        <Image src={PreviewExample} as={Link} to="/article"  />
+                        <Image src={PreviewExample} as={Link} to="/article" />
                     </Grid.Column>
 
                     <Grid.Column>
+                        <Popup position='top right' trigger={
+                            <Icon name='search plus' className="float-right"></Icon>
+                        }>
+                            <Popup.Header>Peek article</Popup.Header>
+                            <Popup.Content>
+                                <p>{loremIpsum(Math.floor((Math.random() * 100) + 10))}</p>
+                            </Popup.Content>
+                        </Popup>
+
                         <Header.Subheader as='h4'>{getCurrentDate()} - {loremIpsum(Math.floor((Math.random() * 10) + 5))}</Header.Subheader>
-                        <Header color="orange"  as={Link} to="/article">
+                        <Header color="orange" as={Link} to="/article">
                             {loremIpsum(Math.floor((Math.random() * 100) + 10))}
                         </Header>
                         <p>{loremIpsum(Math.floor((Math.random() * 100) + 100))}</p>
@@ -25,6 +35,7 @@ const ArticleCard = (props) => {
                 </Grid.Row>
             </Grid>
         </Segment>
+
     )
 }
 
--- src/components/BreakingNews.jsx
+++ src/components/BreakingNews.jsx
@@ -1,44 +1,28 @@
-import React, { useState } from 'react'
-import { Popup, Image, Container, Header, Segment } from 'semantic-ui-react'
+import React from 'react'
+import { Popup, Image, Container, Header, Segment, Icon } from 'semantic-ui-react'
 
 import { breakingArticle } from "../constants/Articles"
 
-const timeoutLength = 2500
-
 const BreakingNews = () => {
-    const [isOpen, setOpen] = useState(false);
-    let timer = null;
 
-    const handleOpen = () => {
-        setOpen(true);
-
-        timer = setTimeout(() => {
-            setOpen(false)
-        }, timeoutLength)
-    }
-
-    const handleClose = () => {
-        setOpen(false);
-        clearTimeout(timer)
-    }
     return (
-        <Popup on='click' open={isOpen} onClose={handleClose} onOpen={handleOpen} position='top right' hideOnScroll //change to mousehover and icon?
-            trigger={
-                <Segment>
-                    <Header as='h2' color="orange">BREAKING NEWS</Header>
-                    <Image src={breakingArticle.imagesrc}></Image>
-                    <Header as='h2' color="orange">{breakingArticle.header}</Header>
-                    <Container>{breakingArticle.description}</Container>
-                </Segment>
-            }>
-
-            <Popup.Header>Peek</Popup.Header>
-            <Popup.Content>
-                <p>{breakingArticle.peek}</p>
-            </Popup.Content>
-        </Popup>
+        <Segment>
+            <div>
+                <Popup position='top right' on="click" trigger={
+                    <Icon name='search plus' className="float-right"></Icon>
+                }>
+                    <Popup.Header>Peek article</Popup.Header>
+                    <Popup.Content>
+                        <p>breaking!</p>
+                    </Popup.Content>
+                </Popup>
+                <Header as='h2' color="orange">BREAKING NEWS</Header>
+            </div>
+            <Image src={breakingArticle.imagesrc}></Image>
+            <Header as='h2' color="orange">{breakingArticle.header}</Header>
+            <Container>{breakingArticle.description}</Container>
+        </Segment>
     )
-
 }
 
 export default BreakingNews
